66e21191093c3f273f61ef6448d6fef8ff8f5211
Antoine-Thomas <searching.murphy@gmail.com>
2024-02-28 17:11:27 +0100

    yut-
    ur

--- app/public/wp-content/docket-cache-data/cachestats.php
+++
@@ -1,8 +0,0 @@
-<?php if ( !\defined('ABSPATH') ) { return false; }
-return [
-    'timestamp' => 1641231644,
-    'size' => 45497,
-    'filesize' => 66474,
-    'files' => 44,
-];
-/*@DOCKET_CACHE_EOF*/
